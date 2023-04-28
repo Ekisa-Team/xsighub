1fd693bb644df92095dbe1f7b6bb6828886ac855
juanmesa2097 <juanmesa2097@gmail.com>
2023-04-27 20:41:44 -0500

    chore: update scripts & config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,4 +17,8 @@ module.exports = {
         es2017: true,
         node: true,
     },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
 };
