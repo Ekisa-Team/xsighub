e61057aae129e8df81335d9f743df8212932361c
juanmesa2097 <juanmesa2097@gmail.com>
2023-05-03 04:33:08 -0500

    feat(mobile): update mobile designs

--- pkg/mobile/src/app/tab3/tab3.page.ts
+++ pkg/mobile/src/app/tab3/tab3.page.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import SignaturePad from 'signature_pad';
 
 @Component({
     selector: 'app-tab3',
     templateUrl: 'tab3.page.html',
     styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {}
+export class Tab3Page implements AfterViewInit {
+    @ViewChild('canvas') canvasRef!: ElementRef;
+
+    signaturePad!: SignaturePad;
+
+    signatureImg!: string;
+
+    ngAfterViewInit() {
+        this.signaturePad = new SignaturePad(this.canvasRef.nativeElement, {
+            backgroundColor: 'white',
+        });
+    }
+
+    save() {
+        const base64Data = this.signaturePad.toDataURL();
+        this.signatureImg = base64Data;
+    }
+
+    undo() {
+        const data = this.signaturePad.toData();
+
+        if (data) {
+            data.pop();
+            this.signaturePad.fromData(data);
+        }
+    }
+
+    clear() {
+        this.signaturePad.clear();
+    }
+}
