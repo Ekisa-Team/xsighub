588e56c683643c84124d37740ba7271713586d90
juanmesa2097 <juanmesa2097@gmail.com>
2023-05-08 13:25:20 -0500

    chore(server): update address

--- pkg/server/src/server.ts
+++ pkg/server/src/server.ts
@@ -14,7 +14,10 @@ if (import.meta.env.PROD) {
             schema,
         });
 
-        app.listen({ port: PORT });
+        app.listen({
+            port: PORT,
+            host: '0.0.0.0',
+        });
     } catch (error) {
         app.log.error(error);
         process.exit(1);
