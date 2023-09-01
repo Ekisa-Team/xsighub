a4139d4327435e54bdbbd8af1ca762e426d49b94
juanmesa2097 <juanmesa2097@gmail.com>
2023-08-31 19:17:43 -0500

    chore: format code

--- apps/server/src/lib/dto/base.dto.ts
+++ apps/server/src/lib/dto/base.dto.ts
@@ -2,19 +2,19 @@ import { Expose } from 'class-transformer';
 import { IsDate, IsNotEmpty } from 'class-validator';
 
 export class BaseDto<PkType> {
-  @Expose({ toClassOnly: true })
-  @IsNotEmpty()
-  readonly id: PkType;
+    @Expose({ toClassOnly: true })
+    @IsNotEmpty()
+    readonly id: PkType;
 }
 
 export class BaseDtoWithTimestamps<PkType> extends BaseDto<PkType> {
-  @Expose({ toClassOnly: true })
-  @IsDate()
-  @IsNotEmpty()
-  createdAt: Date;
+    @Expose({ toClassOnly: true })
+    @IsDate()
+    @IsNotEmpty()
+    createdAt: Date;
 
-  @Expose({ toClassOnly: true })
-  @IsDate()
-  @IsNotEmpty()
-  updatedAt: Date | null;
+    @Expose({ toClassOnly: true })
+    @IsDate()
+    @IsNotEmpty()
+    updatedAt: Date | null;
 }
